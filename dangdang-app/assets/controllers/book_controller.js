dc6af2b3d495b50b4fbcbfab601106acfc6bfa27
lily616263 <1633007184@qq.com>
2016-10-19 21:21:51 +0800

    tabbar-11bottom

--- dangdang-app/assets/controllers/book_controller.js
+++ dangdang-app/assets/controllers/book_controller.js
@@ -1,10 +1,10 @@
 app.controller('bookController',['$scope',function(){
     $scope.bookTypes = [
-        {id:'art',name:'儿童'},
-        {id:'history',name:'历史'},
-        {id:'jobbole',name:'儿童'},
-        {id:'novel',name:'小说'},
-        {id:'technology',name:'技术'},
+        {id:'art',name:'儿童',description:''},
+        {id:'history',name:'历史',description:''},
+        {id:'jobbole',name:'儿童',description:''},
+        {id:'novel',name:'小说',description:''},
+        {id:'technology',name:'技术',description:''},
 
     ]
 }])
--- dangdang-app/assets/controllers/main_controller.js
+++ dangdang-app/assets/controllers/main_controller.js
@@ -1,5 +1,5 @@
 app.controller('mainController',['$scope','commonService',function($scope,c_service){
-// c_service.getData('art',function(res){
-// console.dir(res)
-// })
+c_service.getData('art',function(res){
+console.dir(res)
+})
 }])
