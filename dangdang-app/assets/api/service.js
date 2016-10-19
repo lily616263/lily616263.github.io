1d98a74597a8439064f033e1aa346bd2dffc8050
lily616263 <1633007184@qq.com>
2016-10-19 15:26:27 +0800

    数据提交

---
+++ dangdang-app/assets/api/service.js
@@ -0,0 +1,17 @@
+app.factory('commonService',['$http',function($http){
+    var service ={};
+
+
+    service.getData = function(type,callBack){
+        $http({
+            url:'../../data/book_ +"type"+json',
+            method:'get'
+        })
+        .then(function(res){
+            console.log('success')
+            callBack(res)
+        },function(err){
+            console.dir(err)
+        })
+    }
+}])
