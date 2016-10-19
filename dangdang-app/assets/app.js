23afa4b898a3293e287248307fc977675a5345dc
lily616263 <1633007184@qq.com>
2016-10-19 16:34:05 +0800

    tabber

--- dangdang-app/assets/app.js
+++ dangdang-app/assets/app.js
@@ -1,1 +1,16 @@
 var app =angular.module('app',['ngRoute'])
+app.config(['$routeProvider',function($routeProvider){
+    $routeProvider
+    .when('./',{
+   tempalteUrl:'../tpl/index.html',
+   controller:''
+    })
+    .when('/blog',{
+         tempalteUrl:'../tpl/blog.html',
+   controller:''
+    })
+    .when('/book',{
+         tempalteUrl:'../tpl/book.html',
+   controller:''
+    })
+}])
