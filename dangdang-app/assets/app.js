33dff3a72421b961eda3443ee15374cf8cfc8389
lily616263 <1633007184@qq.com>
2016-10-20 18:23:37 +0800

    qqq

--- dangdang-app/assets/app.js
+++ dangdang-app/assets/app.js
@@ -2,23 +2,23 @@ var app = angular.module('app', ['ngRoute'])
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            tempalteUrl: './assets/tpl/index.html',
+            templateUrl: './assets/tpl/index.html',
             controller: ''
         })
         .when('/blog', {
-            tempalteUrl: './assets/tpl/blog.html',
+            templateUrl: './assets/tpl/blog.html',
             controller: ''
         })
         .when('/book', {
-            tempalteUrl: './assets/tpl/book.html',
+            templateUrl: './assets/tpl/book.html',
             controller: 'bookController'
         })
         .when('/about_me', {
-            tempalteUrl: './assets/tpl/about_me.html',
+            templateUrl: './assets/tpl/about_me.html',
             controller: ''
         })
         .when('/book_list/:id', {
-            templatewUrl: './assets/tpl/book_list.html',
+            templateUrl: './assets/tpl/book_list.html',
             controller: 'bookListController'
         })
         .otherwise({
